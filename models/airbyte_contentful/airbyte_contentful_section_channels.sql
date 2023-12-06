e3895a8213da8933ecca30ce65770136796363fe
david.ashirov <david.ashirov@one.vet>
2023-12-06 11:58:31 -0500

    added sectionChannels

---
+++ models/airbyte_contentful/airbyte_contentful_section_channels.sql
@@ -0,0 +1,27 @@
+{% set source_environment = var('source_environment','contentful_dev') %}
+{% set target_schema = var('target_schema','contentful_delivery_dev') %}
+
+
+{{ config(
+    alias='section_channels',
+    schema=target_schema,
+    dist='id'
+) }}
+
+SELECT
+{% if target.type == 'redshift' %}
+       -- Redshift-specific SQL query
+       id,
+       ("fields"."title")::varchar(150) as channel_title
+{% elif target.type == 'postgres' %}
+    -- PostgreSQL-specific SQL query
+       id,
+       ("fields"->>'title')::varchar(150) as channel_title
+{% endif %}
+from
+     {{ ref('entries') }} as channels
+where
+    content_type = 'sectionChannel'
+
+
+
