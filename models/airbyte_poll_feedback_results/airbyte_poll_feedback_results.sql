b5fe7f4b296dc2f51eaeea6695738780abe45dd9
david.ashirov <david.ashirov@one.vet>
2023-12-16 10:08:38 -0500

    replacement for data-ops/airflow/dags                 /poll_feedback.py

--- models/airbyte_poll_feedback_results/airbyte_poll_feedback_results.sql
+++ models/airbyte_poll_feedback_results/airbyte_poll_feedback_results.sql
@@ -28,5 +28,21 @@ select _airbyte_data."feedback_id"::varchar(36)    as feedback_id,
        (timestamp 'epoch' +
         _airbyte_data."createdAt"::bigint::numeric /
         1000::numeric *
-        interval '1 second')                       as responded_at
-FROM {{ schema }}._airbyte_raw_poll_feedback_results
+        interval '1 second')                       as responded_at,
+       cp.id as poll_id,
+       cp.internal_title,
+       cp.header,
+       cp.campaign,
+       cp.description,
+       cp.options,
+       cp.hide_responses,
+       cp.hide_signup,
+       cp.expert_rederence_id,
+       cpo.option as poll_option,
+       cpo.id as poll_option_id,
+       cpo.ordinal as poll_option_ordinal
+FROM {{ schema }}._airbyte_raw_poll_feedback_results pr
+         JOIN contentful_delivery.polls AS cp
+              ON cp.id = pr._airbyte_data."poll_id"::varchar(36)
+         JOIN contentful_delivery.poll_options AS cpo
+              ON cpo.poll_id = cp.id AND  cpo.id = pr._airbyte_data."response_id"::varchar(36)
